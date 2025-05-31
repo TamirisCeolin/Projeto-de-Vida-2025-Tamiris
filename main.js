81aa9cea1a257b338b8937b52212426603eda7d4
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-05-31 09:59:40 -0300

    31/05

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
   if (tempoFinal > 0) {
     return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos");
   } else {
-    return "Prazo Finalizado";
+    return "Prazo Finalizado!";
   }
 }
 
@@ -52,4 +52,4 @@ function comecaCronometro() {
   setInterval(atualizaCronometro, 1000);
 }
 
-comecaCronometro();
+//comecaCronometro();

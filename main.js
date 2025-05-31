0430890dca363fecdc1e62fcb2e2e5d0cf0a6b09
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-05-31 09:32:20 -0300

    31/05

--- main.js
+++ main.js
@@ -22,15 +22,6 @@ const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function atualizaCronometro() {
-  for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
-  }
-}
-
-atualizaCronometro();
-
-
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
   let tempoFinal = tempoObjetivo - tempoAtual;
@@ -42,6 +33,19 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
-
+  
   return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos");
 }
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();

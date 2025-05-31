08a3bcdf3a469bdf8f7d60301dcbdeebcaac50b9
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-05-31 09:17:20 -0300

    31/05

--- main.js
+++ main.js
@@ -16,9 +16,13 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempos[0]);
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
